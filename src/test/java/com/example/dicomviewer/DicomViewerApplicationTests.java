4e0bb4c91de565ad77d3caa41a5c31cc14824ba1
fbwoduf112 <fbwoduf2@gmail.com>
2025-08-19 16:48:03 +0900

    fasd

---
+++ src/test/java/com/example/dicomviewer/DicomViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dicomviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DicomViewerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
